c156d5a66dc96870a1cbdccde7ae45e117103a37
KsanaSal <oasalivon@gmail.com>
2024-03-01 23:31:54 +0200

    setting project

---
+++ app/component/animation/SplashScreen.tsx
@@ -0,0 +1,9 @@
+const SplashScreen = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            SplashScreen
+        </div>
+    );
+};
+
+export default SplashScreen;
